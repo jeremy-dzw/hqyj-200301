5a1e320a5662f18a8cd193ef7484bf4bf358e66c
hp <hp@LAPTOP-BRKN5TKA>
2020-06-03 14:03:42 +0800

    "firstSubmit"

---
+++ success/src/main/java/hqyj/success/controllerClass.java
@@ -0,0 +1,16 @@
+package hqyj.success;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class controllerClass
+{
+    @ResponseBody
+    @RequestMapping("/success")
+    public String response()
+    {
+        return "you  can  do  what  you  want  to  do......";
+    }
+}
---
+++ successInEclipse/src/main/java/com/hqyj/successInEclipse/SuccessInEclipseApplication.java
@@ -0,0 +1,14 @@
+package com.hqyj.successInEclipse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuccessInEclipseApplication 
+{	
+	public static void main(String[]args)
+	{
+		SpringApplication.run(SuccessInEclipseApplication.class, args);
+	}
+
+}
