121890827091a405d96fa0aea87412bfa1b4266e
jeremy-dzw <2209440250@qq.com>
2020-06-04 09:11:51 +0800

    "MultipleEnvironmentDemo"

---
+++ SpringBootMultipleEnvironments/src/main/java/com/hqyj/MultipleEnvironments/SpringBootMultipleEnvironmentsApplication.java
@@ -0,0 +1,13 @@
+package com.hqyj.MultipleEnvironments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultipleEnvironmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMultipleEnvironmentsApplication.class, args);
+	}
+
+}
