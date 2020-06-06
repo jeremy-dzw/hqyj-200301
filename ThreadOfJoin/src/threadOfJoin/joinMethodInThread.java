4d3c7c1349c5e6b1db0ded78dcc5ec60de410d9a
jeremy-dzw <2209440250@qq.com>
2020-06-06 22:49:49 +0800

    "joinMethodInThread"

--- ThreadOfJoin/src/threadOfJoin/joinMethodInThread.java
+++ ThreadOfJoin/src/threadOfJoin/joinMethodInThread.java
@@ -1,4 +1,29 @@
 package threadOfJoin;
 
-public class joinMethodInThread {
+public class joinMethodInThread implements Runnable
+{
+    public static void main(String[] args)
+    {   joinMethodInThread joinMethodInThread = new joinMethodInThread();
+        Thread thread1 = new Thread(joinMethodInThread,"jeremy");
+        thread1.start();
+        for (int i = 0;i<1000;i++)
+        {   if(i==200)
+            {
+                try
+                {
+                    thread1.join();
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("主程序在运行......");
+        }
+    }
+    @Override
+    public void run()
+    {
+        System.out.println(Thread.currentThread().getName()+"正在运行......");
+    }
 }
