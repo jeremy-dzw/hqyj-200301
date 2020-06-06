98490cbbadbf3b9952df564ff723ba61bfc1a4a4
jeremy-dzw <2209440250@qq.com>
2020-06-06 09:35:14 +0800

    "MutipleThreadCaseWithProblemForInterview"

---
+++ multipleThreadCaseForInterview/src/threadCase/MutipleThreadCaseForInterview.java
@@ -0,0 +1,28 @@
+package threadCase;
+/**
+ * 多线程并发处理同一个资源时，存在严重问题
+ * **/
+public class MutipleThreadCaseForInterview implements Runnable
+{   int ticketNumber = 10;
+    @Override
+    public void run()
+    {
+        while(true)
+        {
+            if(ticketNumber > 0)
+                System.out.println(Thread.currentThread().getName()+"拿到了第"+ticketNumber-- +"张票");
+            else
+                break;
+        }
+    }
+    public static void main(String[] args)
+    {
+        MutipleThreadCaseForInterview t= new MutipleThreadCaseForInterview();
+        Thread t1 = new Thread(t,"小明");
+        Thread t2 = new Thread(t,"小红");
+        Thread t3 = new Thread(t,"黄牛党");
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
